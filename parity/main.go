fdf948de841ede95b9a7c5a29d39091fd9f2d360
Rogério Peixoto <rcbpeixoto@gmail.com>
2020-12-01 11:04:50 +0000

    parity challenge

---
+++ parity/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	integerSlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for _, number := range integerSlice {
+		if number%2 == 0 {
+			fmt.Printf("%d is even\n", number)
+		} else {
+			fmt.Printf("%d is odd\n", number)
+		}
+	}
+}
