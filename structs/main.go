2b71ad55e4d952320f779cf0b370b81320fe7c53
Rogério Peixoto <rcbpeixoto@gmail.com>
2020-12-01 14:13:49 +0000

    structs and pointers

---
+++ structs/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func main() {
+	var alex person
+	var contact contactInfo
+	contact.email = "alex.anderson@gmail.com"
+	contact.zipCode = 5555555
+	// alex := person{
+	// 	 firstName: "Alex",
+	//   lastName: "Anderson",
+	//   contact: contactInfo {
+	//	 	 email: "jim@gmail.com",
+	//  	 zipCode:90000,
+	//   }
+	// },
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	alex.contactInfo = contact
+	alexPointer := &alex
+	alexPointer.updateName("alexy")
+	alex.print()
+}
